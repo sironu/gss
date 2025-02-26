e450feeda93ba90bfffd64dd6c64ef0f55c51317
Emmanuel Onu <sireconu@gmail.com>
2025-02-26 18:03:48 +0100

    added mobile nav links

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+let toggleMobileNav = document.querySelector(".mobile-nav-link-button");
+let mobileNavContainer = document.querySelector(".mobile-nav-container");
+let mobileNavs = document.querySelectorAll(".mobile-nav-link");
+
+toggleMobileNav.onclick = function () {
+  mobileNavContainer.classList.toggle("media-h-0");
+};
+
+for(let mobileNav of mobileNavs) {
+  mobileNav.onclick = function () {
+    mobileNavContainer.classList.toggle("media-h-0");
+  };
+
+}
+let backToTop = document.querySelector(".backToTop");
+
+backToTop.onclick = function () {
+  scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })
+  
+}
+
+
+
+
+
 let main = document.querySelector("main");
 let footer = document.querySelector("footer");
 let loader = document.querySelector(".loader-section");
@@ -13,7 +41,6 @@ function gradeStudent() {
   let roundomNum3 = Math.floor(Math.random() * 7);
   let roundomNum4 = Math.floor(Math.random() * 7);
   let roundomNum5 = Math.floor(Math.random() * 7);
-
   let roundomNum6 = Math.floor(Math.random() * 7);
   let roundomNum7 = Math.floor(Math.random() * 7);
   let roundomNum8 = Math.floor(Math.random() * 7);
@@ -27,7 +54,6 @@ function gradeStudent() {
   let score3 = scores[roundomNum3];
   let score4 = scores[roundomNum4];
   let score5 = scores[roundomNum5];
-
   let score6 = scores[roundomNum6];
   let score7 = scores[roundomNum7];
   let score8 = scores[roundomNum8];
@@ -36,14 +62,11 @@ function gradeStudent() {
   let score11 = scores[roundomNum11];
   let score12 = scores[roundomNum12];
 
-
-  
   let grade1 = document.querySelector(".grade1");
   let grade2 = document.querySelector(".grade2");
   let grade3 = document.querySelector(".grade3");
   let grade4 = document.querySelector(".grade4");
   let grade5 = document.querySelector(".grade5");
-
   let grade6 = document.querySelector(".grade6");
   let grade7 = document.querySelector(".grade7");
   let grade8 = document.querySelector(".grade8");
@@ -52,30 +75,24 @@ function gradeStudent() {
   let grade11 = document.querySelector(".grade11");
   let grade12 = document.querySelector(".grade12");
 
-  grade1.innerHTML = score1
-  grade2.innerHTML = score2
-  grade3.innerHTML = score3
-  grade4.innerHTML = score4
-  grade5.innerHTML = score5
-
-  grade6.innerHTML = score6
-  grade7.innerHTML = score7
-  grade8.innerHTML = score8
-  grade9.innerHTML = score9
-  grade10.innerHTML = score10
-  grade11.innerHTML = score11
-  grade12.innerHTML = score12
-  
+  grade1.innerHTML = score1;
+  grade2.innerHTML = score2;
+  grade3.innerHTML = score3;
+  grade4.innerHTML = score4;
+  grade5.innerHTML = score5;
+  grade6.innerHTML = score6;
+  grade7.innerHTML = score7;
+  grade8.innerHTML = score8;
+  grade9.innerHTML = score9;
+  grade10.innerHTML = score10;
+  grade11.innerHTML = score11;
+  grade12.innerHTML = score12;
 }
 
 function validateForm() {
   let selectYear = document.querySelector("#select-year").value;
   let myName = document.querySelector("#my-name").value;
   let myPassword = document.querySelector(".my-password").value;
-
-  selectYear.innerHTML = "";
-  myName.innerHTML = "";
-  myPassword.innerHTML = "";
 
   if (selectYear === "") {
     alert("Kindly Select Exam Year");
@@ -89,6 +106,9 @@ function validateForm() {
   if (myPassword.length != "11") {
     alert("Kindly Enter Correct Password");
   } else {
+    selectYear.innerHTML = "";
+    myName.innerHTML = "";
+    myPassword.innerHTML = "";
     studentName.innerHTML = myName;
     header.classList.add("hidden");
     main.classList.add("hidden");
